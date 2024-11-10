b10753d3bf177ee09f0293a30b72a5659f6c89a5
Jon Andre <jasolber@stud.ntnu.no>
2024-11-10 16:53:10 +0100

    Changed min_tls_version

--- modules/storage/main.tf
+++ modules/storage/main.tf
@@ -13,6 +13,7 @@ resource "azurerm_storage_account" "sa" {
   location                 = var.location
   account_tier             = "Standard"
   account_replication_type = "GRS"
+  min_tls_version          = "TLS1_2"
 }
 
 resource "azurerm_storage_container" "storage_container" {
