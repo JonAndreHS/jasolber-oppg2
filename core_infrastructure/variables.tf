26a01e4b986ec9b78dd4b43f1f12a35706249aed
Jon Andre <jasolber@stud.ntnu.no>
2024-11-11 19:45:27 +0100

    Added defaults to variables.tf

--- core_infrastructure/variables.tf
+++ core_infrastructure/variables.tf
@@ -1,9 +1,11 @@
 variable "rgName" {
   description = "The name of the resource group"
   type        = string
+  default     = "rg-ja-oblig2"
 }
 
 variable "location" {
   description = "The location of the resource group"
   type        = string
+  default     = "WestEurope"
 }
