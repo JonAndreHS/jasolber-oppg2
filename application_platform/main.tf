3c4e8426eda0645ad358371080173309f6413fc0
Jon Andre <jasolber@stud.ntnu.no>
2024-11-12 15:40:01 +0100

    Testing github actions

--- application_platform/main.tf
+++ application_platform/main.tf
@@ -20,7 +20,7 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "rg" {
-    name     = var.rgName
+    name     = "${var.rgName}-${terraform.workspace}"
     location = var.location
 }
 
--- application_platform/variables.tf
+++ application_platform/variables.tf
@@ -1,9 +1,11 @@
 variable "rgName" {
   description = "Name of the resource group"
   type        = string
+  default     = "rg-ja-oblig2"    
 }
 
 variable "location" {
   description = "Location of the resource group"
   type        = string
+  default     = "WestEurope"
 }
